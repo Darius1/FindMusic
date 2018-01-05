5369dca389287f512f2e8fd42acef3b098481675
Darius McFarland <indycolts1893@gmail.com>
2018-01-05 16:47:51 -0500

    Removed old code that got merged into the current program

--- Search.java
+++ Search.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -389,72 +388,3 @@ public class Search {
     	new Search(args);
     }
 }
-=======
-/**
- * This class will handle all of the search functionality in the FindMusic program
- */
-public class Search {
-    /** Holds the artist name */
-    private String artist;
-    /** Holds the website url that will be searched */
-    private String website;
-    /** Holds the song title */
-    private String song;
-
-    /**
-     * Search object constructor that takes in the artist name as a parameter
-     * @param artist the artist's name
-     */
-    public Search(String artist) {
-        this.artist = artist;
-        website = "";
-        song = "";
-    }
-
-    /**
-     * Returns the artist's name
-     * @return artist
-     */
-    public String getArtist() {
-        return artist;
-    }
-
-    /**
-     * Returns the website name
-     * @return website
-     */
-    public String getWebsite() {
-        return website;
-    }
-
-    /**
-     * Returns the song title
-     * @return song
-     */
-    public String getSong() {
-        return song;
-    }
-
-    /**
-     * Creates a string in the format "Artist: Song"
-     * @return string
-     */
-    public String toString() {
-        return artist + ": " + song;
-    }
-
-    /**
-     * Tests the functionality of the Search class
-     * @param args not used
-     */
-
-    /**
-    public static void main(String[] args) {
-        Search test = new Search("Earl");
-        System.out.println(test.getArtist());
-        System.out.println(test.getSong());
-        System.out.println(test);
-    }
-    */
-}
->>>>>>> refs/remotes/origin/master
