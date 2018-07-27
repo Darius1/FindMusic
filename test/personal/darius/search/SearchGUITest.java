c194acc62ef5a57a1cd167e6695294faec53710b
Darius McFarland <indycolts1893@gmail.com>
2018-07-27 18:24:06 -0400

    Imported TestFX

--- test/personal/darius/search/SearchGUITest.java
+++ test/personal/darius/search/SearchGUITest.java
@@ -6,30 +6,33 @@ import java.util.concurrent.TimeoutException;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-public class SearchGUITest {
+public class SearchGUITest extends ApplicationTest {
 
-//	@Before
-//	public void setup() throws TimeoutException {
-//		FxToolkit.registerPrimaryStage();
-//    	FxToolkit.showStage();
-//    	
-//    	FxToolkit.setupApplication(SearchGUI.class);
-//	}
-//	
-//	/**
-//	 * Tests the default search option and only displays songs for today
-//	 *
-//	 */
-//	@Test
-//	public void testDefaultSearch() {
-////		assertThat(Long.valueOf(1), instanceOf(Long.class));
-//	    	
-////	   this.clickOn("Search");
-////	   this.clickOn("No");
-////	   this.clickOn("Return to Main Menu");
-//	 }
+	@Before
+	public void setup() throws TimeoutException {
+		FxToolkit.registerPrimaryStage();
+    	FxToolkit.showStage();
+    	
+    	FxToolkit.setupApplication(SearchGUI.class);
+	}
+	
+	/**
+	 * Tests the default search option and only displays songs for today
+	 *
+	 */
+	@Test
+	public void testDefaultSearch() {
+		assertThat(Long.valueOf(1), instanceOf(Long.class));    	
+		
+		this.clickOn("Search");
+		this.clickOn("No");
+		this.clickOn("Return to Main Menu");
+	 }
 
 }
